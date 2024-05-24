165bd4a76f043ede9efb3798cd5f711ca5e76be7
Nathane <nathane.miranda@eagletecnologia.com.br>
2024-05-24 11:07:50 -0300

    Ajustado calculos e impressão

--- script.js
+++ script.js
@@ -1,30 +1,74 @@
 document.addEventListener("DOMContentLoaded", () => {
   const display = document.getElementById("display");
 
+  const TAMANHO_MAXIMO_DIPLAY = 21;
+
   function adicionarNumero(numero) {
-    if (display.innerText.length < 22) {
+    if (display.innerText.length < TAMANHO_MAXIMO_DIPLAY) {
       display.innerText += numero;
     }
   }
 
-  function adicionarVirgula() {
-    if (display.innerText.length < 22) {
-      display.innerText += ",";
+  function adicionarPonto() {
+    if (display.innerText.length < TAMANHO_MAXIMO_DIPLAY) {
+      let parcelas = display.innerText.split(/[/*+-]/);
+
+      if (!parcelas[parcelas.length - 1].includes(".")) {
+        display.innerText += ".";
+      }
     }
   }
 
   function adicionarOperador(operador) {
-    if (display.innerText.length < 22) {
-      display.innerText += operador;
+    if (display.innerText.length < TAMANHO_MAXIMO_DIPLAY) {
+      if (
+        /[/*+-]/.test(display.innerText.charAt(display.innerText.length - 1))
+      ) {
+        display.innerText = display.innerText.slice(0, -1) + operador;
+      } else {
+        if (!/[/*+-]/.test(display.innerText)) {
+          if (display.innerText.length === 0) {
+            display.innerText = "0" + operador;
+          } else {
+            display.innerText += operador;
+          }
+        }
+      }
     }
   }
 
   function calcular() {
+    if (display.innerText.length === 0) {
+      return;
+    }
     try {
-      let resultado = eval(display.innerText.replace("÷", "/"));
-      display.innerText = resultado.toString().substring(0, 22);
+      let resultado;
+      let parcelas = display.innerText.split(/[/*+-]/);
+
+      if (parcelas[1].length === 0 || parcelas[1] == ".") {
+        parcelas[1] = 0;
+      }
+
+      if (display.innerText.includes("/")) {
+        resultado = parseFloat(parcelas[0]) / parseFloat(parcelas[1]);
+      }
+      if (display.innerText.includes("*")) {
+        resultado = parseFloat(parcelas[0]) * parseFloat(parcelas[1]);
+      }
+      if (display.innerText.includes("+")) {
+        resultado = parseFloat(parcelas[0]) + parseFloat(parcelas[1]);
+      }
+      if (display.innerText.includes("-")) {
+        resultado = parseFloat(parcelas[0]) - parseFloat(parcelas[1]);
+      }
+
+      if (resultado.toString().length > TAMANHO_MAXIMO_DIPLAY) {
+        throw new Error("Tamanho display excedido");
+      } else {
+        display.innerText = resultado.toString();
+      }
     } catch (e) {
-      display.innerText = "Erro";
+      display.innerText = e.message;
     }
   }
 
@@ -36,5 +80,5 @@ document.addEventListener("DOMContentLoaded", () => {
   window.adicionarOperador = adicionarOperador;
   window.calcular = calcular;
   window.limpar = limpar;
-  window.adicionarVirgula = adicionarVirgula;
+  window.adicionarPonto = adicionarPonto;
 });
