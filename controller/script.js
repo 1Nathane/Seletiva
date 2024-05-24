b3e8b7101fa5a995c6d0f65db2c68ca9a8c60c93
Nathane <nathane.miranda@eagletecnologia.com.br>
2024-05-24 14:48:25 -0300

    formatado codigo

--- controller/script.js
+++ controller/script.js
@@ -1,6 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const display = document.getElementById("display");
-  const TAMANHO_MAXIMO_DIPLAY = 21;
+  const TAMANHO_MAXIMO_DIPLAY = 15;
+  const UM = 1;
+  const UM_NEGATIVO = -1;
+  const ZERO = 0;
+  const ZERO_STRING = "0";
+  const MULTIPLICADOR = "*";
+  const DIVISOR = "/";
+  const ADITOR = "+";
+  const SUBTRATOR = "-";
+  const PONTO = ".";
+  const OPERADORES = /[/*+-]/;
+  const MSG_ERRO_TAMANHO_EXCEDIDO = "Tamanho display excedido";
+  const VAZIO = "";
 
   function adicionarNumero(numero) {
     if (display.innerText.length < TAMANHO_MAXIMO_DIPLAY) {
@@ -10,32 +22,35 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function adicionarPonto() {
     if (display.innerText.length < TAMANHO_MAXIMO_DIPLAY) {
-      let parcelas = display.innerText.split(/[/*+-]/);
+      let parcelas = display.innerText.split(OPERADORES);
 
-      if (!parcelas[parcelas.length - 1].includes(".")) {
-        display.innerText += ".";
+      if (!parcelas[parcelas.length - UM].includes(PONTO)) {
+        display.innerText += PONTO;
       }
     }
   }
 
   function adicionarOperador(operador) {
-    if (display.innerText.length < TAMANHO_MAXIMO_DIPLAY) {
-      display.innerText = tratarTextoParaReceberOperador(
-        display.innerText,
-        operador
-      );
-    }
+    display.innerText = tratarTextoParaReceberOperador(
+      display.innerText,
+      operador
+    );
   }
 
   function tratarTextoParaReceberOperador(texto, operador) {
-    if (/[/*+-]/.test(texto.charAt(texto.length - 1))) {
-      return texto.slice(0, -1) + operador;
-    }
-    if (texto.length === 0) {
-      return "0" + operador;
+    if (OPERADORES.test(texto.charAt(texto.length - UM))) {
+      return texto.slice(ZERO, UM_NEGATIVO) + operador;
     }
 
-    if (/[/*+-]/.test(texto)) {
+    if (texto.length >= TAMANHO_MAXIMO_DIPLAY) {
+      return texto;
+    }
+
+    if (texto.length === ZERO) {
+      return ZERO_STRING + operador;
+    }
+
+    if (OPERADORES.test(texto)) {
       return texto;
     } else {
       return texto + operador;
@@ -43,39 +58,42 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function calcular() {
-    if (display.innerText.length === 0) {
+    if (display.innerText.length === ZERO) {
+      return;
+    }
+
+    if (!OPERADORES.test(display.innerText)) {
       return;
     }
     try {
       let resultado;
-      let parcelas = display.innerText.split(/[/*+-]/);
-      let casasdecimais = 0;
-      if (parcelas[0].includes(".")) {
-        casasdecimais = quantidadeCasasDecimais(parcelas[0]);
-      }
-      if (parcelas[1].includes(".")) {
-        casasdecimais += quantidadeCasasDecimais(parcelas[1]);
+      let parcelas = display.innerText.split(OPERADORES);
+      let casasdecimais = ZERO;
+      casasdecimais = quantidadeCasasDecimais(parcelas[ZERO]);
+      if (parcelas.length > 1) {
+        casasdecimais += quantidadeCasasDecimais(parcelas[UM]);
+
+        if (parcelas[UM].length === ZERO || parcelas[UM] == PONTO) {
+          parcelas[UM] = ZERO;
+        }
       }
 
-      if (parcelas[1].length === 0 || parcelas[1] == ".") {
-        parcelas[1] = 0;
+      if (display.innerText.includes(DIVISOR)) {
+        resultado = parseFloat(parcelas[ZERO]) / parseFloat(parcelas[UM]);
       }
-
-      if (display.innerText.includes("/")) {
-        resultado = parseFloat(parcelas[0]) / parseFloat(parcelas[1]);
+      if (display.innerText.includes(MULTIPLICADOR)) {
+        resultado = parseFloat(parcelas[ZERO]) * parseFloat(parcelas[UM]);
       }
-      if (display.innerText.includes("*")) {
-        resultado = parseFloat(parcelas[0]) * parseFloat(parcelas[1]);
+      if (display.innerText.includes(ADITOR)) {
+        resultado = parseFloat(parcelas[ZERO]) + parseFloat(parcelas[UM]);
       }
-      if (display.innerText.includes("+")) {
-        resultado = parseFloat(parcelas[0]) + parseFloat(parcelas[1]);
-      }
-      if (display.innerText.includes("-")) {
-        resultado = parseFloat(parcelas[0]) - parseFloat(parcelas[1]);
+      if (display.innerText.includes(SUBTRATOR)) {
+        resultado = parseFloat(parcelas[ZERO]) - parseFloat(parcelas[UM]);
       }
 
       if (resultado.toString().length > TAMANHO_MAXIMO_DIPLAY) {
-        throw new Error("Tamanho display excedido");
+        display.innerText = resultado.toString().length;
+        throw new Error(MSG_ERRO_TAMANHO_EXCEDIDO);
       } else {
         display.innerText = resultado.toFixed(casasdecimais).toString();
       }
@@ -85,11 +103,14 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function limpar() {
-    display.innerText = "";
+    display.innerText = VAZIO;
   }
 
   function quantidadeCasasDecimais(texto) {
-    return texto.split(".")[1].length;
+    if (!texto.includes(PONTO)) {
+      return ZERO;
+    }
+    return texto.split(PONTO)[UM].length;
   }
 
   window.adicionarNumero = adicionarNumero;
